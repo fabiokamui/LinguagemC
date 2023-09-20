a3a60fa409dc2280702704f10409574e1a3bf41f
fabio <fabio.silva910@etec.sp.gov.br>
2023-09-20 20:24:09 -0300

    fabio

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf ("Boa Noite!\n");
+	printf("Prova na proxima semana :)\n");
+	printf("sem consulta");
+	return 0;
+}
